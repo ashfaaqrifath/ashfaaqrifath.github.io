689de05a7ac7b72d97f82290848b561347ce36e5
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2023-04-15 13:03:23 +0530

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById("load").style.animation =
-		"wipe-out-left cubic-bezier(0.25, 1, 0.3, 1) 510ms";
+		"wipe-out-left cubic-bezier(0.25, 1, 0.3, 1) 1000ms";
 	setTimeout(() => {
 		document.getElementById("load").style.display = "none";
 	}, 500);
